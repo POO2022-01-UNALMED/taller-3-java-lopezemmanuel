4579952e5acc4948b5849074eba5eaf7a4081f95
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-03-26 17:15:14 -0500

    Intento 1

---
+++ src/src/taller3/televisores/Control.java
@@ -0,0 +1,51 @@
+package taller3.televisores;
+
+public class Control {
+	private TV tv;
+	
+	public Control(TV tv) {
+		this.tv = tv;
+		this.tv.setControl(this);
+	}
+	
+	// ----- ACCESO REMOTO -----
+	public void turnOn() {
+		this.tv.turnOn();
+	}
+	
+	public void turnOff() {
+		this.tv.turnOff();
+	}
+	
+	public void canalUp() {
+		this.tv.canalUp();
+	}
+	
+	public void canalDown() {
+		this.tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		this.tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		this.tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		if(this.tv.getEstado() && canal >= 1 && canal <= 120) {
+			this.tv.setCanal(canal);
+		}
+	}
+	
+	// ----- GET Y SET -----
+	public TV getTV() {
+		return this.tv;
+	}
+	
+	public void setTV(TV tv) {
+		this.tv = tv;
+	}
+
+}
